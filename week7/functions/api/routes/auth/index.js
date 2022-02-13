e95e0861bc187e6d808ef26f6af40cac61f34836
jeonghoon <shb03323@naver.com>
2022-02-13 23:40:12 +0900

    [ADD] level 1 - authentication

--- week7/functions/api/routes/auth/index.js
+++ week7/functions/api/routes/auth/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signup', require('./authSignupPOST'));
-// router.post('/login/email', require('./authLoginEmailPOST'));
+router.post('/login/email', require('./authLoginEmailPOST'));
 
 module.exports = router;
--- week7/functions/api/routes/post/index.js
+++ week7/functions/api/routes/post/index.js
@@ -1,8 +1,9 @@
 const express =require('express');
+const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 router.get("/", require('./postListGET'));
-router.get("/:postId", require('./postGET'));
+router.get("/:postId", checkUser, require('./postGET'));
 router.put("/:postId", require('./postPUT'));
 router.delete("/:postId", require('./postDELETE'));
 router.post("/", require('./postPOST'));
